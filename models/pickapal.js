53d5237abd801a6cbbfa17b60ebc60d05dcf4a12
thomson3914 <thomson3914@gmail.com>
2018-09-03 08:28:39 -0400

    added seeds.sql and table in pickapal.js

--- models/pickapal.js
+++ models/pickapal.js
@@ -1,7 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
   var Pickapal = sequelize.define("Pickapal", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    name: DataTypes.STRING,
+      image: DataTypes.STRING,
+      q0: DataTypes.STRING,
+      q1: DataTypes.STRING,
+      q2: DataTypes.STRING,
+      q3: DataTypes.STRING,
+      q4: DataTypes.STRING,
+      q5: DataTypes.STRING,
+      q6: DataTypes.STRING,
   });
   return Pickapal;
 };
+
+
+// module.exports = function(sequelize, DataTypes) {
+//   var Friend = sequelize.define("Friend", {
+//       name: DataTypes.STRING,
+//       image: DataTypes.STRING,
+//       q0: DataTypes.STRING,
+//       q1: DataTypes.STRING,
+//       q2: DataTypes.STRING,
+//       q3: DataTypes.STRING,
+//       q4: DataTypes.STRING,
+//       q5: DataTypes.STRING,
+//       q6: DataTypes.STRING,
+//       q7: DataTypes.STRING,
+//       q8: DataTypes.STRING,
+//       q9: DataTypes.STRING
+//   });
+
+//   return Friend;
+// };
